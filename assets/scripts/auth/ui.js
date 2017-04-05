66921782e13125f4b47ed2837b61711981631a14
antleo1995 <antleo1995@gmail.com>
2017-04-04 21:12:55 -0400

    Tic Tac Toe Authentication feature
    Add bare bones working authentication feature. Literally 100% copy
    pasta from working code. :)
    
    From here planning to finagle these forms behind some modals. Seems
    the easiest way for me to hide them easily.
    
    P.s. thank you bootstrap. <3

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,40 @@
+'use strict'
+const store = require('../store.js')
+
+const signUpSuccess = (data) => {
+  console.log(data)
+}
+
+const signUpFailure = (error) => {
+  console.error(error)
+}
+const signInSuccess = (data) => {
+  console.log('SignIn success ran. Data is: ', data)
+  store.user = data.user
+}
+const signInFailure = (error) => {
+  console.error('SignIn failure ran. Error is: ', error)
+}
+const signOutSuccess = () => {
+  console.log('SignOut success ran. Data is: ', store)
+  store.user = null
+}
+const signOutFailure = (error) => {
+  console.error('SignOut failure ran. Error is: ', error)
+}
+const changePasswordSuccess = () => {
+  console.log('Change Password. Data is: ', store)
+}
+const changePasswordFailure = (error) => {
+  console.error('Change Password failure ran. Error is: ', error)
+}
+module.exports = {
+  signUpSuccess,
+  signUpFailure,
+  signInSuccess,
+  signInFailure,
+  signOutSuccess,
+  signOutFailure,
+  changePasswordSuccess,
+  changePasswordFailure
+}

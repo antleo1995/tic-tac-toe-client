cef0f6accb5c2f2e7817e539d173969d937a59ea
antleo1995 <antleo1995@gmail.com>
2017-04-07 16:48:53 -0400

    TIC TAC TOE Deploy
    Still needed to update prod URL.
    
    production: 'https://aqueous-atoll-85096.herokuapp.com/'
    
    That hopefully works.

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -3,7 +3,7 @@
 const config = {
   apiOrigins: {
     // production: 'https://ga-wdi-boston.herokuapp.com'
-    production: 'https://antleo1995.github.io'
+    production: 'https://aqueous-atoll-85096.herokuapp.com/'
   }
 }
 

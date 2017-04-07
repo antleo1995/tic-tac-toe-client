1f21bb2edcbd05e88c207253be976145b7c7729e
antleo1995 <antleo1995@gmail.com>
2017-04-07 16:45:14 -0400

    TIC TAC TOE Deploy
    Changing production URL.

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,8 @@
 
 const config = {
   apiOrigins: {
-    production: 'https://ga-wdi-boston.herokuapp.com'
+    // production: 'https://ga-wdi-boston.herokuapp.com'
+    production: 'https://antleo1995.github.io'
   }
 }
 

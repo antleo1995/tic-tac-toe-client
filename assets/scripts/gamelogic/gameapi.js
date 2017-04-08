655526421a40de742574c2f74dccf965d3db2f0a
antleo1995 <antleo1995@gmail.com>
2017-04-08 11:43:55 -0400

    TIC TAC TOE cleanup
    Cleans up gameapi.js

--- assets/scripts/gamelogic/gameapi.js
+++ assets/scripts/gamelogic/gameapi.js
@@ -42,36 +42,6 @@ const getAllGames = (data) => {
     data
   })
 }
-
-/*
-const signIn = (data) => {
-  return $.ajax({
-    url: config.apiOrigin + '/sign-in',
-    method: 'POST',
-    data
-  })
-}
-const signOut = () => {
-  return $.ajax({
-    url: config.apiOrigin + '/sign-out/' + store.user.id,
-    method: 'DELETE',
-    headers: {
-      Authorization: 'Token token=' + store.user.token
-    }
-  })
-}
-const changePassword = (data) => {
-  console.log('Data is: ', data)
-  return $.ajax({
-    url: config.apiOrigin + '/change-password/' + store.user.id,
-    method: 'PATCH',
-    headers: {
-      Authorization: 'Token token=' + store.user.token
-    },
-    data
-  })
-}
-*/
 module.exports = {
   createGame,
   updateGame,
